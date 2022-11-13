c0d39893b8bc1dd36e96d8462f69a99ab5d95a81
mhmodmre <74112435+mhmodmre@users.noreply.github.com>
2022-11-13 21:56:01 +0200

    Add files via upload

---
+++ JS – מבוא – פונקציות ותנאים/script.js
@@ -0,0 +1,14 @@
+// let arr = [];
+// for (let i = 0; i < 2; i++) {
+//   arr.push(prompt("enter {i + i} "));
+// }
+// arr.forEach((val) => {
+//   console.log(val);
+// });
+
+// if (arr > i) {
+//   alert = "אין שארית";
+// } else arr < i;
+// {
+//   alert = "אין שארית";
+// }
